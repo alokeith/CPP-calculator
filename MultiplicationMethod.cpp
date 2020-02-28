f1a77356c865f6a5a2de9f9bb4f9d4c615a7fc50
alokeith <61555834+alokeith@users.noreply.github.com>
2020-02-28 23:47:47 +0800

    Add files via upload

---
+++ MultiplicationMethod.cpp
@@ -0,0 +1,17 @@
+//Multiplication Method
+#include <iostream>
+using namespace std;
+
+int main(){
+int a, b;
+  cout << "Enter first number: ";
+  cin >> a;
+  cout << "Enter second number: ";
+  cin >> b;
+  
+  int c = a * b;
+  
+  cout << "The Product is: " << c;
+  
+  return 0;
+  }
